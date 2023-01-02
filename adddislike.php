0369e3f0620d92c4d406bdd2a56a996aa95e50de
ValiaDourou <valiadourou@gmail.com>
2023-01-02 02:03:29 +0200

    exw valei epithdes sto leaderboards ths arxikhs ws path to evaluationadmin kai sto about thw arxikhs tou user to evaluationuser gia na mporei na treksei(mhn agxwneste tha mpei se button sto pop up tou thoma)

---
+++ adddislike.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include 'dbconn.php';
+$con = openDB();
+
+$o = $_POST['o'];
+$t=mysqli_query($con, "INSERT INTO userlikes VALUES(".$_SESSION['id'].",$o,'DISLIKE')");
+
+closeDB($con);
+
+
+?>
