39a9daebca166233bb7e9c23b4f181f9a5c804b9
Mpooks <jimbalafas2001@gmail.com>
2023-01-03 20:22:32 +0200

    a
    a

--- statistics.js
+++ statistics.js
@@ -1,19 +1,18 @@
 const data = {
-    labels: ['2022-08-03', '2022-08-04', '2022-10-06', '2022-10-07', '2022-12-04', '2022-12-05', '2023-01-04'],
-    datasets: [{
-      label: 'Total Oferrs',
-      data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        'rgba(25, 25, 112, 0.2)',
+  labels: ['2022-08-03', '2022-08-04', '2022-10-06', '2022-10-07', '2022-12-04', '2022-12-05', '2023-01-04'],
+  datasets: [{
+    label: 'Total Offers per Month',
+    data: [18, 12, 6, 9, 12, 3, 9],
+    backgroundColor: [
+      'rgba(1, 169, 172, 1)',
+    ],
+    borderColor: [
+      'rgba(25, 25, 112, 1)',
 
-      ],
-      borderColor: [
-        'rgba(25, 25, 112, 1)',
-        
-      ],
-      borderWidth: 1
-    }]
-  };
+    ],
+    borderWidth: 1
+  }]
+};
 
   // config 
   const config = {
@@ -22,17 +21,26 @@ const data = {
     options: {
       scales: {
         x:{ 
-            min: '2022-01-01',
+            min: '2022-01-05',
             max: '2022-12-31',
             type: 'time',
             time: {
-                unit: 'day'
-            }
+              unit: 'day',
+            },
+            ticks: {
+              source: 'ticks',
+              unitstepSize: 1,
+              autoSkip: true,
+          },
         },
         y: {
           beginAtZero: true,
-          suggestedmin: '0',
-          suggestedmax: '20'
+          min: 0,
+          max: 20,
+          ticks: {
+            // forces step size to be 50 units
+            stepSize: 1
+          }
         }
       }
     }
@@ -44,78 +52,50 @@ const data = {
     config
   );
 
-  const weekChart = new Chart(
-    document.getElementById('weekChart'),
-    config
-  );
+  function filterChart(date){
+    var dateErr = document.getElementById('valid');
+    console.log(date.value);
+    const year = date.value.substring(0, 4);
+    const month = date.value.substring(5, 7);
+    
 
+    
+    const lastDay = (y,m) => {
+      return new Date(y, m, 0).getDate()
+    };
 
-  const yearSelect = document.getElementById('year');
-  year= new Date().getFullYear();
-  
-  for (let i = 0; i <= 100; i++) {
-    const option = document.createElement('option');
-    option.textContent = year - i;
-    option.value = year - i;
-    yearSelect.appendChild(option);
+    lastDay(year, month);
+
+    const startDate = `${date.value}-01`; 
+    const endDate = `${date.value}-${lastDay(year, month)}`;
+    console.log(endDate);
+    console.log(startDate);
+    myChart.config.options.scales.x.min = startDate;
+    myChart.config.options.scales.x.max = endDate;
+    myChart.update();
+
+    var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+
+    if(!regex_date.test(startDate) || !regex_date.test(endDate) || year < 1000 || year > 3000 || month == 0 || month > 12)
+    {
+      myChart.config.options.scales.x.min = '2022-01-01';
+      myChart.config.options.scales.x.max = '2022-12-31';
+      myChart.update();
+      dateErr.innerHTML = "Invalid date.";
+    }
+
+    if(date.value === ""){
+      myChart.config.options.scales.x.min = '2022-01-01';
+      myChart.config.options.scales.x.max = '2022-12-31';
+      myChart.update();
+      dateErr.innerHTML = "Invalid date.";
+  }
+    
   }
 
-
-function filterChart(){
-
-  const monthSelect = document.getElementById('month');
-  console.log(monthSelect);
-  console.log(monthSelect.value);
-
-
-
-  const lastDay = (y,m) => {
-    return new Date(y, m, 0).getDate()
-  };
-
-  var y=yearSelect.value;
-  var m=monthSelect.value;
-  const date=y.toString()+'-'+m.toString();
-
-  lastDay(yearSelect.value, monthSelect.value);
-  const startDate = `${date}-01`;
-  const endDate = `${date}-${lastDay(yearSelect.value, monthSelect.value)}`; 
-  console.log(endDate);
-  console.log(startDate);
-  myChart.config.options.scales.x.min = startDate;
-  myChart.config.options.scales.x.max = endDate;
-  myChart.update();
-}
-
-function filterYearChart(){
-
-  console.log(yearSelect.value);
-  const monthSelect = document.getElementById('month');
-  console.log(monthSelect.value);
-
-  const lastDay = (y,m) => {
-    return new Date(y, m, 0).getDate()
-  };
-
-  var y=yearSelect.value;
-  var m=monthSelect.value;
-  const date=y.toString()+'-'+m.toString();
-
-  lastDay(yearSelect.value, monthSelect.value);
-  const startDate = `${date}-01`;
-  const endDate = `${date}-${lastDay(yearSelect.value, monthSelect.value)}`; 
-  console.log(endDate);
-  console.log(startDate);
-  myChart.config.options.scales.x.min = startDate;
-  myChart.config.options.scales.x.max = endDate;
-  myChart.update();
-  
-}
-
-
-function reset(){
-  myChart.config.options.scales.x.min = '2022-01-01';
-  myChart.config.options.scales.x.max = '2022-12-31';
-  myChart.update();
-}
+  function reset(){
+    myChart.config.options.scales.x.min = '2022-01-01';
+    myChart.config.options.scales.x.max = '2022-12-31';
+    myChart.update();
+  }
 
