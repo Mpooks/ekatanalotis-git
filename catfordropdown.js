88c7e14b489e5f3738c8632bcb7981edfa3d3bc2
ValiaDourou <valiadourou@gmail.com>
2023-01-04 04:05:37 +0200

    pls na mhn exei allaksei kati

--- catfordropdown.js
+++ catfordropdown.js
@@ -1,5 +1,9 @@
 async function categoriesfd(){
 const select = document.getElementById("selectcat");
+const select2 = document.getElementById("fad");
+
+const v = document.createElement('select');
+select2.appendChild(v);
 
 const response = await fetch('./categoriesfordropdown.php');
     
@@ -11,6 +15,7 @@ for (let i = 0; i < data.length; i++) {
   op.value=data[i].cname;
   op.text=data[i].cname;
   select.appendChild(op);
+  v.appendChild(op);
 }
 
 }
