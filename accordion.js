6a6f7114d1b2d0df511fdc4372d2d51181d8bf7d
Mpooks <jimbalafas2001@gmail.com>
2023-01-05 21:44:17 +0200

    gdmknhdcjksmldcjsxldcfjckdcfljnekdmq
    sssmkkpksdsdk[smksdmks[[

--- accordion.js
+++ accordion.js
@@ -42,28 +42,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     });
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    var acc = document.getElementsByClassName("third-level-menu");
-    var panel = document.getElementsByClassName('third-level-panel');
-    for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-    var setClasses = !this.classList.contains('active');
-    setClass(acc, 'active', 'remove');
-    setClass(panel, 'show', 'remove');
-    if (setClasses) {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-    console.log('ax de travixtika  ')
-    }
-    }
-    }
-    function setClass(els, className, fnName) {
-    for (var i = 0; i < els.length; i++) {
-    els[i].classList[fnName](className);
-    }
-    }
-    });
+    document.addEventListener("DOMContentLoaded", function(event) {
+        var acc = document.getElementsByClassName("third-level-menu");
+        var panel = document.getElementsByClassName('third-level-panel');
+        for (var i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+        if (setClasses) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+        console.log('ax de travixtika  ')
+        }
+        }
+        }
+        function setClass(els, className, fnName) {
+        for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+        }
+        }
+        });
 
+
+
+
+
+    
 
 
 
