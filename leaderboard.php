f6a6a2750796215b1a8ac562afe04f360d1b8bb7
ValiaDourou <valiadourou@gmail.com>
2023-01-05 04:31:59 +0200

    mhtso kserw tha me miseis aaaalla tha xreiastei na ftiakseis ligo to css sta leaderboards (kane style kai to koumpi den thelw na sou svhsw tipota pou thes sth css)+++ epishs nai allakse to js giati h allh malakia den leitougouse me to dynamic gamw to spiti tou

--- leaderboard.php
+++ leaderboard.php
@@ -3,7 +3,7 @@ session_start();
 include 'dbconn.php';
 $con = openDB();
 
-$user = mysqli_query($con, "SELECT username,total_score FROM users ORDER BY total_score DESC");
+$user = mysqli_query($con, "SELECT @rn:=@rn+1 AS rank1,username,total_score FROM ( SELECT username,total_score FROM users ORDER BY total_score DESC) t1, (SELECT @rn:=0) t2;");
 $arr = array();
 if (mysqli_num_rows($user) > 0) {
     while ($u = mysqli_fetch_assoc($user)) {
@@ -13,6 +13,5 @@ if (mysqli_num_rows($user) > 0) {
 
 echo json_encode($arr, JSON_UNESCAPED_UNICODE);
 closeDB($con);
-
 
 ?>
