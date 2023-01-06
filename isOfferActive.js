b6d7a4161869cee9085bd963b13becc97be57250
ValiaDourou <valiadourou@gmail.com>
2023-01-06 03:26:28 +0200

    kanonika prepei na einai etoimo to moirasma twn tokens,ananewsh tokens kai total score kathe mhna

--- isOfferActive.js
+++ isOfferActive.js
@@ -1,6 +1,23 @@
 async function isItActive(){
 
     const response = await fetch('./activeornot.php');
-   
+
+
+    const date = new Date();
+    function isLastDayOfMonth(date = new Date()) {
+
+        const oneDayInMs = 1000 * 60 * 60 * 24;
+      
+        return new Date(date.getTime() + oneDayInMs).getDate() === 1;
+      }
+      if(isLastDayOfMonth(date)===1){
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let currentDate = `${year}-${month}-${day}`;
+        var formData = new FormData();
+        formData.append('thismonth', currentDate);
+        const response = await fetch('./splitTok.php',{ method: 'POST', body: formData });
+      }
 
 }
